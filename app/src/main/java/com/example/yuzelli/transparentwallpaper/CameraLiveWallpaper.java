e58e10049a57bfb9e1ad0ed96e87bf089cfaf08b
yuzelli <zsl5155@qq.com>
2017-05-27 17:01:26 +0800

    实现动态播放mv

--- app/src/main/java/com/example/yuzelli/transparentwallpaper/CameraLiveWallpaper.java
+++ app/src/main/java/com/example/yuzelli/transparentwallpaper/CameraLiveWallpaper.java
@@ -1,93 +1,127 @@
 package com.example.yuzelli.transparentwallpaper;
 
+import android.app.WallpaperManager;
+import android.content.BroadcastReceiver;
+import android.content.ComponentName;
+import android.content.Context;
+import android.content.Intent;
+import android.content.IntentFilter;
+import android.content.res.AssetFileDescriptor;
+import android.content.res.AssetManager;
+import android.graphics.ImageFormat;
 import android.hardware.Camera;
+import android.media.MediaPlayer;
 import android.service.wallpaper.WallpaperService;
+import android.util.Log;
 import android.view.MotionEvent;
 import android.view.SurfaceHolder;
-import java.io.IOException;
 
-/**
- * Created by 51644 on 2017/5/17.
- */
+import java.io.IOException;
 
 public class CameraLiveWallpaper extends WallpaperService {
     // 实现WallpaperService必须实现的抽象方法
     public Engine onCreateEngine() {
         // 返回自定义的CameraEngine
-        return new CameraEngine();
+        return new VideoEngine();
+    }
+    public static final String VIDEO_PARAMS_CONTROL_ACTION = "com.example.yuzelli.transparentwallpaper";
+    public static final String KEY_ACTION = "action";
+    public static final int ACTION_VOICE_SILENCE = 110;
+    public static final int ACTION_VOICE_NORMAL = 111;
+
+    public static void voiceSilence(Context context) {
+        Intent intent = new Intent(CameraLiveWallpaper.VIDEO_PARAMS_CONTROL_ACTION);
+        intent.putExtra(CameraLiveWallpaper.KEY_ACTION, CameraLiveWallpaper.ACTION_VOICE_SILENCE);
+        context.sendBroadcast(intent);
+    }
+
+    public static void voiceNormal(Context context) {
+        Intent intent = new Intent(CameraLiveWallpaper.VIDEO_PARAMS_CONTROL_ACTION);
+        intent.putExtra(CameraLiveWallpaper.KEY_ACTION, CameraLiveWallpaper.ACTION_VOICE_NORMAL);
+        context.sendBroadcast(intent);
+    }
+
+    public static void setToWallPaper(Context context) {
+        final Intent intent = new Intent(WallpaperManager.ACTION_CHANGE_LIVE_WALLPAPER);
+        intent.putExtra(WallpaperManager.EXTRA_LIVE_WALLPAPER_COMPONENT,
+                new ComponentName(context, CameraLiveWallpaper.class));
+        context.startActivity(intent);
     }
 
 
-    class CameraEngine extends Engine implements Camera.PreviewCallback {
-        private Camera camera;
 
+    class VideoEngine extends Engine {
+
+        private MediaPlayer mMediaPlayer;
+
+        private BroadcastReceiver mVideoParamsControlReceiver;
         @Override
         public void onCreate(SurfaceHolder surfaceHolder) {
             super.onCreate(surfaceHolder);
+            IntentFilter intentFilter = new IntentFilter(VIDEO_PARAMS_CONTROL_ACTION);
+            registerReceiver(mVideoParamsControlReceiver = new BroadcastReceiver() {
+                @Override
+                public void onReceive(Context context, Intent intent) {
+                    int action = intent.getIntExtra(KEY_ACTION, -1);
 
-            startPreview();
-            // 设置处理触摸事件
-            setTouchEventsEnabled(true);
+                    switch (action) {
+                        case ACTION_VOICE_NORMAL:
+                            mMediaPlayer.setVolume(1.0f, 1.0f);
+                            break;
+                        case ACTION_VOICE_SILENCE:
+                            mMediaPlayer.setVolume(0, 0);
+                            break;
 
+                    }
+                }
+            }, intentFilter);
         }
 
         @Override
-        public void onTouchEvent(MotionEvent event) {
-            super.onTouchEvent(event);
-            // 时间处理:点击拍照,长按拍照
-        }
+        public void onSurfaceCreated(SurfaceHolder holder) {
+            Log.d("-->", "VideoEngine#onSurfaceCreated ");
+            super.onSurfaceCreated(holder);
+            mMediaPlayer = new MediaPlayer();
+            mMediaPlayer.setSurface(holder.getSurface());
+            try {
+                AssetManager assetMg = getApplicationContext().getAssets();
+                AssetFileDescriptor fileDescriptor = assetMg.openFd("myTest.mp4");
+                mMediaPlayer.setDataSource(fileDescriptor.getFileDescriptor(),
+                        fileDescriptor.getStartOffset(), fileDescriptor.getLength());
+                mMediaPlayer.setLooping(true);
+                mMediaPlayer.setVolume(1.0f, 1.0f);
+                mMediaPlayer.prepare();
+                mMediaPlayer.start();
 
-        @Override
-        public void onDestroy() {
-            super.onDestroy();
-            stopPreview();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+
         }
 
         @Override
         public void onVisibilityChanged(boolean visible) {
+            Log.d("-->", "VideoEngine#onVisibilityChanged visible = " + visible);
             if (visible) {
-                startPreview();
+                mMediaPlayer.start();
             } else {
-                stopPreview();
-            }
-        }
-
-        /**
-         * 开始预览
-         */
-        public void startPreview() {
-            camera = Camera.open();
-            camera.setDisplayOrientation(90);
-
-            try {
-                camera.setPreviewDisplay(getSurfaceHolder());
-            } catch (IOException e) {
-                e.printStackTrace();
-            }
-            camera.startPreview();
-
-        }
-
-        /**
-         * 停止预览
-         */
-        public void stopPreview() {
-            if (camera != null) {
-                try {
-                    camera.stopPreview();
-                    camera.setPreviewCallback(null);
-                    // camera.lock();
-                    camera.release();
-                } catch (Exception e) {
-                    e.printStackTrace();
-                }
-                camera = null;
+                mMediaPlayer.pause();
             }
         }
 
         @Override
-        public void onPreviewFrame(byte[] bytes, Camera camera) {
-            camera.addCallbackBuffer(bytes);
+        public void onSurfaceDestroyed(SurfaceHolder holder) {
+            Log.d("-->", "VideoEngine#onSurfaceDestroyed ");
+            super.onSurfaceDestroyed(holder);
+            mMediaPlayer.release();
+            mMediaPlayer = null;
+
+        }
+
+        @Override
+        public void onDestroy() {
+            unregisterReceiver(mVideoParamsControlReceiver);
+            super.onDestroy();
         }
     }
 }
